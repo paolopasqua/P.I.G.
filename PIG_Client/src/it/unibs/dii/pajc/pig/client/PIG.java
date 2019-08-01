d7f6e04d0f3c484f2f8e43fd317f1503eb087dee
paolopasqua <paolo.pasquali@outlook.it>
2019-08-02 00:11:51 +0200

    Edits main class to remove test and manage exceptions in ChoiceDataSource initialization; Adds localization to PIG class

--- PIG_Client/src/it/unibs/dii/pajc/pig/client/PIG.java
+++ PIG_Client/src/it/unibs/dii/pajc/pig/client/PIG.java
@@ -2,71 +2,43 @@ package it.unibs.dii.pajc.pig.client;
 
 import it.unibs.dii.pajc.pig.client.controller.ChoiceManager;
 import it.unibs.dii.pajc.pig.client.model.ChoiceDataSource;
-import it.unibs.dii.pajc.pig.client.utility.ServerConnectionData;
 import it.unibs.dii.pajc.pig.client.view.ConnectionForm;
 import it.unibs.dii.pajc.pig.client.view.HelpForm;
 
-import java.util.Date;
+import javax.swing.*;
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class PIG {
+
+    private static ResourceBundle localizationBundle = ResourceBundle.getBundle("localization/PIG");
+
     public static void main(String[] args) {
         HelpForm helpForm = new HelpForm(); //TODO: init help form
         ConnectionForm choiceView = new ConnectionForm();
-        ChoiceDataSource choiceModel = new ChoiceDataSource();
         ChoiceManager choiceController = new ChoiceManager();
+        ChoiceDataSource choiceModel = null;
+        try {
+            choiceModel = new ChoiceDataSource();
+            choiceModel.load(); //TODO: manage exceptions
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
 
-        choiceModel.load(); //TODO: manage exceptions
-
-        //TODO: remove test
-        ServerConnectionData data = null;
-
-        data = new ServerConnectionData();
-        data.setAddress("192.168.0.99");
-        data.setDescription("Diskstation");
-        data.setLastConnection(new Date(19, 1, 13));
-        data.setFavorite(true);
-        choiceModel.addElement(data);
-
-        data = new ServerConnectionData();
-        data.setAddress("192.168.0.43");
-        data.setDescription(null);
-        data.setLastConnection(new Date());
-        data.setFavorite(false);
-        choiceModel.addElement(data);
-
-        data = new ServerConnectionData();
-        data.setAddress("192.168.0.74");
-        data.setDescription("Portatile");
-        data.setLastConnection(new Date(19, 4, 22));
-        data.setFavorite(false);
-        choiceModel.addElement(data);
-
-        data = new ServerConnectionData();
-        data.setAddress("192.168.0.23");
-        data.setDescription(null);
-        data.setLastConnection(new Date(17, 12, 31));
-        data.setFavorite(false);
-        choiceModel.addElement(data);
-
-        data = new ServerConnectionData();
-        data.setAddress("192.168.0.1");
-        data.setDescription("Gateway");
-        data.setLastConnection(new Date());
-        choiceModel.addElement(data);
-
-        data = new ServerConnectionData();
-        data.setAddress("0.0.0.0");
-        data.setDescription("Localhost");
-        data.setLastConnection(new Date(19, 8, 1));
-        data.setFavorite(true);
-        choiceModel.addElement(data);
-        //end test
+            //TODO: localize
+            JOptionPane.showMessageDialog(null, localizationBundle.getString("advice.error.loading.message"), localizationBundle.getString("advice.error.loading.title"), JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        ChoiceDataSource modelFinalRef = choiceModel;
 
         //choiceController.attachHelpView(helpForm);
         choiceController.setView(choiceView);
         choiceController.setModel(choiceModel);
         choiceController.addClosingActionListener(actionEvent -> {
-            choiceModel.store();
+            try {
+                modelFinalRef.store();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
 
         choiceController.start();
